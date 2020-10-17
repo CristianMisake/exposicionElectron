fc8d90d30df907439f09c79e46ff8c45194d20e1
Cristian Grondona <cristiangrondon@gmail.com>
2020-10-16 22:05:09 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const { app, BrowserWindow } = require('electron');
+
+const url = require('url');
+const path = require('path');
+
+let mainWindow;
+
+if(process.env.NODE_ENV !== 'production') {
+    require('electron-reload')(__dirname, {
+        electron: path.join(__dirname, '../node_modules', '.bin', 'electron')
+    });
+}
+
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({ width:720, height:600 });
+
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'views/index.html'),
+        protocol: 'file',
+        slashes: true,
+    }))
+});
